5df586a555a7a8a1404f59f7006b5bf0edf413b5
Chiao F <php@html.js.cn>
2020-03-01 11:00:06 +0800

    simply play hardcoded stream

--- app/src/main/java/com/pedro/vlctestapp/MainActivity.java
+++ app/src/main/java/com/pedro/vlctestapp/MainActivity.java
@@ -2,25 +2,21 @@ package com.pedro.vlctestapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.SurfaceView;
-import android.view.View;
 import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 import com.pedro.vlc.VlcListener;
 import com.pedro.vlc.VlcVideoLibrary;
-import java.util.Arrays;
 import org.videolan.libvlc.MediaPlayer;
+import java.util.Arrays;
 
 /**
  * Created by pedro on 25/06/17.
  */
-public class MainActivity extends AppCompatActivity implements VlcListener, View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements VlcListener {
 
   private VlcVideoLibrary vlcVideoLibrary;
-  private Button bStartStop;
-  private EditText etEndpoint;
 
   private String[] options = new String[]{":fullscreen"};
 
@@ -30,9 +26,6 @@ public class MainActivity extends AppCompatActivity implements VlcListener, View
     getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
     setContentView(R.layout.activity_main);
     SurfaceView surfaceView = (SurfaceView) findViewById(R.id.surfaceView);
-    bStartStop = (Button) findViewById(R.id.b_start_stop);
-    bStartStop.setOnClickListener(this);
-    etEndpoint = (EditText) findViewById(R.id.et_endpoint);
     vlcVideoLibrary = new VlcVideoLibrary(this, this, surfaceView);
     vlcVideoLibrary.setOptions(Arrays.asList(options));
   }
@@ -43,25 +36,28 @@ public class MainActivity extends AppCompatActivity implements VlcListener, View
   }
 
   @Override
+  public void onResume() {
+    vlcVideoLibrary.play("rtsp://ipc:554/stream1");;
+    super.onResume();
+  }
+
+  @Override
+  public void onPause() {
+    Log.i("app", "onPause");
+    if (vlcVideoLibrary.isPlaying()) {
+      vlcVideoLibrary.stop();
+    }
+    super.onPause();
+  }
+
+  @Override
   public void onError() {
     Toast.makeText(this, "Error, make sure your endpoint is correct", Toast.LENGTH_SHORT).show();
     vlcVideoLibrary.stop();
-    bStartStop.setText(getString(R.string.start_player));
   }
 
   @Override
   public void onBuffering(MediaPlayer.Event event) {
 
-  }
-
-  @Override
-  public void onClick(View view) {
-    if (!vlcVideoLibrary.isPlaying()) {
-      vlcVideoLibrary.play(etEndpoint.getText().toString());
-      bStartStop.setText(getString(R.string.stop_player));
-    } else {
-      vlcVideoLibrary.stop();
-      bStartStop.setText(getString(R.string.start_player));
-    }
   }
 }
